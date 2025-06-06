deeb5039bd58d99df20fd3e6af3de83c6cbbee83
Joseph <mwanikij118@gmail.com>
2025-06-06 15:07:25 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+function saturdayFun(activity ) {
+   if (activity !== undefined) {
+        activity = 'roller-skate';
+    }
+    if (activity === 'roller-skate') {
+        return `This Saturday, I want to bathe my dog!`;
+    }
+    if (activity === 'bathe my dog') {
+        return `This Saturday, I want to bathe my dog!`;
+    } else {
+        return `This Saturday, I want to roller-skate!`;
+    }
+}
+
+let mondayWork = function(activity ) {
+    if (activity !== undefined) {
+       activity = 'go to the office';
+    }
+    if (activity === 'go to the office') {    
+        return `This Monday, I will work from home.`
+    }
+    if (activity === 'work from home') {
+        return `This Monday, I will work from home.`
+    } else {
+        return `This Monday, I will go to the office.`
+    }
+}
+    
+let wrapAdjective = function(style = '*') {
+    return function(adjective = 'special') {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
